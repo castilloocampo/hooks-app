4670710548debcd184c7bfe20905018fcbbe8f26
Mario Castillo <castilloocampomario@outlook.com>
2022-05-20 18:57:56 -0500

    Testing todoReducer

---
+++ src/tests/components/08-useReducer/todoReducer.test.js
@@ -0,0 +1,50 @@
+import { demoTodos } from '../../fixtures/demoTodos';
+import todoReducer from '../../../components/08-useReducer/todoReducer';
+
+describe('todoReducer', () => {
+  test('should return default state', () => {
+    const state = todoReducer(demoTodos, {});
+    expect(state).toEqual(demoTodos);
+  });
+
+  test('should add a ToDo element', () => {
+    const newTodo = {
+      id: new Date().getTime(),
+      descripcion: "Learn Express",
+      done: false
+    };
+
+    const action = {
+      type: 'ADD_TODO',
+      payload: newTodo
+    }
+    const state = todoReducer(demoTodos, action);
+    expect(state.length).toBe(3);
+    expect(state).toEqual([...demoTodos, newTodo]);
+  });
+
+  test('should delete a ToDo element', () => {
+    const action = {
+      type: 'DELETE_TODO',
+      payload: 2
+    }
+
+    const state = todoReducer(demoTodos, action);
+
+    expect(state.length).toBe(1);
+    expect(state).toEqual(state.filter(({ id }) => id !== 2));
+  });
+
+  test('should be toggle ToDo element', () => {
+    const [oldTodo] = demoTodos;
+    console.log(oldTodo);
+    const action = {
+      type: 'TOGGLE_TODO',
+      payload: 1
+    }
+
+    const [todo] = todoReducer(demoTodos, action);
+    expect(todo.done).toBe(!oldTodo.done);
+
+  });
+});
