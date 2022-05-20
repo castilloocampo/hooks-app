077f2dd818094bb200955523d31a8bb8432c25a9
Mario Castillo <castilloocampomario@outlook.com>
2022-05-20 13:27:18 -0500

    tests in useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 
-const UseFetch = (url = "") => {
+const useFetch = (url = "") => {
   const isMounted = useRef(true);
 
   const initialState = { data: null, loading: true, error: null };
@@ -10,27 +10,30 @@ const UseFetch = (url = "") => {
     return () => {
       isMounted.current = false;
     }
-  }, [])
-
-
+  }, []);
+  
   useEffect(() => {
-    setState(initialState)
+    setState(initialState);
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        isMounted.current && setState({
-          loading: false,
-          error: null,
-          data
+    .then(response => response.json())
+    .then(data => {
+        isMounted.current && setState(() => {
+          return {
+            loading: false,
+            error: null,
+            data
+          }
+        });
+      })
+      .catch(()=>{
+        setState(() => {
+          return {
+            data: null,
+            loading: false,
+            error: "No se pudo cargar la info"
+          }
         })
       })
-      .catch(err => {
-        isMounted.current && setState({
-          loading: false,
-          error: err,
-          data: null
-        })
-      });
 
   }, [url])
 
@@ -38,4 +41,4 @@ const UseFetch = (url = "") => {
 
 }
 
-export default UseFetch;
+export default useFetch;
---
+++ src/tests/hooks/useFetch.test.js
@@ -0,0 +1,33 @@
+import { cleanup, renderHook, waitFor } from '@testing-library/react';
+import useFetch from '../../hooks/useFetch';
+describe('useFetch', () => {
+  beforeEach(() => {
+    cleanup();
+    jest.setTimeout(2000);
+  })
+
+  test('should return default information', () => {
+    const { result } = renderHook(() => useFetch('https://www.breakingbadapi.com/api/quotes/1'));
+    const { data, loading, error } = result.current;
+    expect(data).toBe(null);
+    expect(loading).toBe(true);
+    expect(error).toBe(null);
+  });
+
+  test('should return required info', async () => {
+    const { result } = renderHook(() => useFetch('https://www.breakingbadapi.com/api/quotes/1'));
+    await waitFor(() => {
+      expect(result.current.data.length).toBe(1);
+      expect(result.current.loading).not.toBeTruthy();
+      expect(result.current.error).toBe(null);
+    });
+  });
+
+  test('should return error', async () => {
+    const { result } = renderHook(() => useFetch('https://reqres.in/apid/users?page=2'));
+    console.log(result.current);
+    await waitFor(() => {
+      expect(result.current.error).toBe('No se pudo cargar la info');
+    });
+  });
+});
